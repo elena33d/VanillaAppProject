34ea5b12d2c0f826869fbe451035ea51e365051c
elena <o.didenko@student.bachelor-goit.com.ua>
2026-02-03 02:14:10 +0100

    Deploy fitness app

--- js/filters.js
+++
@@ -1,8 +0,0 @@
-const filterButtons = document.querySelectorAll('.filterBtn');
-
-filterButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    filterButtons.forEach(btn => btn.classList.remove('isActive'));
-    button.classList.add('isActive');
-  });
-});
